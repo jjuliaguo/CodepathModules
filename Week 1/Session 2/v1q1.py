1b27ccb88f847eb550ad59f648bfa32e2ec1c21e
Julia Guo <JuliaGuoJuliaGuo@gmail.com>
2026-06-05 00:33:45 -0400

    session 2 work

---
+++ Week 1/Session 2/v1q1.py
@@ -0,0 +1,44 @@
+#Question 1: Transpose Matrix
+# 
+# Write a function transpose() that accepts a 2D integer array matrix 
+# and returns the transpose of matrix. The transpose of a matrix is the 
+# matrix flipped over its main diagonal, swapping the rows and columns.
+
+#3x3 matrix before and after being transposed
+
+
+def transpose(matrix):
+    #python good for string manipulation
+    #built in function?
+
+    #use a for loop to iterate through the rows and then columns
+    #
+    pass
+
+#Example Usage
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print(transpose(matrix))
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+print(transpose(matrix))
+
+#Example Output:
+
+#[
+   # [1, 4, 7],
+   # [2, 5, 8],
+  #  [3, 6, 9]
+# ]
+# [
+   # [1, 4],
+  #  [2, 5],
+  #  [3, 6]
+#]
